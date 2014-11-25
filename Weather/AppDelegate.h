e36bedefd7b03861fdba8fc168af4bdfdf15badc
Ilya Puchka <ilya@puchka.me>
2014-11-26 00:00:34 +0300

    project created

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Ilya Puchka on 25.11.14.
+//  Copyright (c) 2014 Ilya Puchka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Weather
+//
+//  Created by Ilya Puchka on 25.11.14.
+//  Copyright (c) 2014 Ilya Puchka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
